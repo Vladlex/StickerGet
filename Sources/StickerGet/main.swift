81c810b7e1c4dfde566eeee4ce44f7f8d930b579
Vladlex <vladlexion@gmail.com>
2017-11-03 18:03:51 +0300

    Researching bot

--- Sources/StickerGet/main.swift
+++ Sources/StickerGet/main.swift
@@ -1,14 +1,33 @@
 import Foundation
 import TelegramBotSDK
+import SwiftyJSON
 
-let token = readToken(from: "HELLO_BOT_TOKEN")
+public struct JsonAsIs: JsonConvertible {
+    public var json: JSON
+    
+    public init(json: JSON) {
+        self.json = json
+    }
+}
+
+let token = readToken(from: "Token")
 let bot = TelegramBot(token: token)
+
+extension TelegramBot {
+    public func getStickerSet(name: String) -> JsonAsIs {
+        return self.requestSync("getStickerSet", ["name":name])!
+    }
+}
 
 while let update = bot.nextUpdateSync() {
     if let message = update.message, let from = message.from, let text = message.text {
+        let stickerSetName = text
+        let json: JsonAsIs = bot.getStickerSet(name: "Animalssd")
         bot.sendMessageAsync(chat_id: from.id,
                              text: "Hi \(from.first_name)! You said: \(text).\n")
     }
 }
 
-fatalError("Server stopped due to error: \(bot.lastError)")
+let errorDescription = bot.lastError?.debugDescription ?? "Unknown"
+
+fatalError("Server stopped due to error: \(errorDescription)")
